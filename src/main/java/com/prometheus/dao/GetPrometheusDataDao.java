9d214ccaa1aed3b119f223c5d3eb57d7f082d8ba
dt-tianba <455124491@qq.com>
2020-01-16 09:57:30 +0800

    通过注解的方式读取配置文件，抽取工具类

--- src/main/java/com/prometheus/dao/GetPrometheusDataDao.java
+++ src/main/java/com/prometheus/dao/GetPrometheusDataDao.java
@@ -1,37 +1,10 @@
 package com.prometheus.dao;
 
 
-import com.prometheus.service.GetPrometheusDataService;
-import com.prometheus.service.impl.GetPrometheusDataServiceImpl;
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.util.EntityUtils;
+import com.prometheus.utils.GetHttpJsonUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Repository;
-
-import java.io.*;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.text.SimpleDateFormat;
-import java.util.*;
 
 @Repository
 @PropertySource("classpath:entry.properties")
@@ -39,119 +12,9 @@ public class GetPrometheusDataDao {
 
     @Value("${prometheus.data_change_bydate_mock.url}")
     private String url;
-    // utf-8字符编码
-    public static final String CHARSET_UTF_8 = "utf-8";
 
-    // 连接管理器
-    private static PoolingHttpClientConnectionManager pool;
-
-    // 请求配置
-    private static RequestConfig requestConfig;
     public String getValue(){
-        String tempdata=HttpGet(url);
+        String tempdata= GetHttpJsonUtil.HttpGet(url);
         return  tempdata;
-    }
-
-    static {
-
-        try {
-            SSLContextBuilder builder = new SSLContextBuilder();
-            builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
-            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
-                    builder.build());
-            // 配置同时支持 HTTP 和 HTPPS
-            Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory> create().register(
-                    "http", PlainConnectionSocketFactory.getSocketFactory()).register(
-                    "https", sslsf).build();
-            // 初始化连接管理器
-            pool = new PoolingHttpClientConnectionManager(
-                    socketFactoryRegistry);
-            // 将最大连接数增加到200，实际项目最好从配置文件中读取这个值
-            pool.setMaxTotal(200);
-            // 设置最大路由
-            pool.setDefaultMaxPerRoute(2);
-            // 根据默认超时限制初始化requestConfig
-            int socketTimeout = 10000;
-            int connectTimeout = 10000;
-            int connectionRequestTimeout = 10000;
-            requestConfig = RequestConfig.custom().setConnectionRequestTimeout(
-                    connectionRequestTimeout).setSocketTimeout(socketTimeout).setConnectTimeout(
-                    connectTimeout).build();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (KeyStoreException e) {
-            e.printStackTrace();
-        } catch (KeyManagementException e) {
-            e.printStackTrace();
-        }
-
-
-        // 设置请求超时时间
-        requestConfig = RequestConfig.custom().setSocketTimeout(50000).setConnectTimeout(50000)
-                .setConnectionRequestTimeout(50000).build();
-    }
-
-    public static CloseableHttpClient getHttpClient() {
-
-        CloseableHttpClient httpClient = HttpClients.custom()
-                // 设置连接池管理
-                .setConnectionManager(pool)
-                // 设置请求配置
-                .setDefaultRequestConfig(requestConfig)
-                // 设置重试次数
-                .setRetryHandler(new DefaultHttpRequestRetryHandler(0, false))
-                .build();
-
-        return httpClient;
-    }
-    private static String  sendHttpGet(HttpGet httpGet){
-        CloseableHttpClient httpClient = null;
-        CloseableHttpResponse response = null;
-        // 响应内容
-        String responseContent = null;
-        try {
-            // 创建默认的httpClient实例.
-            httpClient = getHttpClient();
-            // 配置请求信息
-            httpGet.setConfig(requestConfig);
-            // 执行请求
-            response = httpClient.execute(httpGet);
-            // 得到响应实例
-            HttpEntity entity = response.getEntity();
-
-            // 判断响应状态
-            if (response.getStatusLine().getStatusCode() >= 300) {
-                throw new Exception(
-                        "HTTP Request is not success, Response code is " + response.getStatusLine().getStatusCode());
-            }
-
-            if (HttpStatus.SC_OK == response.getStatusLine().getStatusCode()) {
-                responseContent = EntityUtils.toString(entity, CHARSET_UTF_8);
-                EntityUtils.consume(entity);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                // 释放资源
-                if (response != null) {
-                    response.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return responseContent;
-    }
-    public static String HttpGet(String httpUrl){
-        //创建Get请求
-        HttpGet httpGet=new HttpGet(httpUrl);
-        return sendHttpGet(httpGet);
-    }
-    public static void main(String[] args) {
-//        HashMap<String,Object> list=new GetPrometheusDataServiceImpl().getValue();
-//        System.out.println(list.toString());
-
     }
 }
