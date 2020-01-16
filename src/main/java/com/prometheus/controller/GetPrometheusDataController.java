1a7e1bc0d3187b6d15cd73766e797bedf8b0ed44
dt-tianba <455124491@qq.com>
2020-01-16 10:28:34 +0800

    添加部分注释

--- src/main/java/com/prometheus/controller/GetPrometheusDataController.java
+++ src/main/java/com/prometheus/controller/GetPrometheusDataController.java
@@ -9,13 +9,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 
+/**
+ * 获取Prometheus数据信息controller层
+ */
 @Controller
 @RequestMapping("/prometheus")
 public class GetPrometheusDataController {
 
     @Autowired
-    GetPrometheusDataService getPrometheusDataService;
+    private GetPrometheusDataService getPrometheusDataService;
 
+    /**
+     * 获取具体数据信息并以json格式进行返回
+     * @return
+     */
     @ResponseBody
     @GetMapping("/getJson")
     public HashMap<String,Object> getJson(){
