d3342bdf9915a1baee3e600ad9bbefd95ff0c5eb
dt-tianba <455124491@qq.com>
2020-01-16 10:29:06 +0800

    添加部分注释

--- src/main/java/com/prometheus/dao/GetPrometheusDataDao.java
+++ src/main/java/com/prometheus/dao/GetPrometheusDataDao.java
@@ -6,13 +6,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Repository;
 
+/**
+ * 获取Prometheus数据信息dao层
+ */
 @Repository
 @PropertySource("classpath:entry.properties")
 public class GetPrometheusDataDao {
 
+    //传输json数据url
     @Value("${prometheus.data_change_bydate_mock.url}")
     private String url;
 
+    /**
+     * 根据url获取具体的json数据并返回
+     * @return
+     */
     public String getValue(){
         String tempdata= GetHttpJsonUtil.HttpGet(url);
         return  tempdata;
