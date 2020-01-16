22c21b167c3384d8b605b6ba19f9dd6fd7a2e333
dt-tianba <455124491@qq.com>
2020-01-16 10:30:27 +0800

    添加部分注释

--- src/main/java/com/prometheus/service/GetPrometheusDataService.java
+++ src/main/java/com/prometheus/service/GetPrometheusDataService.java
@@ -2,6 +2,14 @@ package com.prometheus.service;
 
 import java.util.HashMap;
 
+/**
+ * 获取Prometheus数据信息service层
+ */
 public interface GetPrometheusDataService {
+
+    /**
+     * 获取具体的json数据，并对数据进行相应的处理后进行返回
+     * @return
+     */
     HashMap<String,Object> getValue();
 }
