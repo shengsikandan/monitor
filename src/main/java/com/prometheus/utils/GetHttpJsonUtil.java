1309135ad5326abc7eeab972ff9545417d2173bb
dt-tianba <455124491@qq.com>
2020-01-16 09:58:37 +0800

    新增通过Http获取Json工具类

---
+++ src/main/java/com/prometheus/utils/GetHttpJsonUtil.java
@@ -0,0 +1,133 @@
+package com.prometheus.utils;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+public class GetHttpJsonUtil {
+    // utf-8字符编码
+    public static final String CHARSET_UTF_8 = "utf-8";
+
+    // 连接管理器
+    private static PoolingHttpClientConnectionManager pool;
+
+    // 请求配置
+    private static RequestConfig requestConfig;
+
+    static {
+
+        try {
+            SSLContextBuilder builder = new SSLContextBuilder();
+            builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+                    builder.build());
+            // 配置同时支持 HTTP 和 HTPPS
+            Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory> create().register(
+                    "http", PlainConnectionSocketFactory.getSocketFactory()).register(
+                    "https", sslsf).build();
+            // 初始化连接管理器
+            pool = new PoolingHttpClientConnectionManager(
+                    socketFactoryRegistry);
+            // 将最大连接数增加到200，实际项目最好从配置文件中读取这个值
+            pool.setMaxTotal(200);
+            // 设置最大路由
+            pool.setDefaultMaxPerRoute(2);
+            // 根据默认超时限制初始化requestConfig
+            int socketTimeout = 10000;
+            int connectTimeout = 10000;
+            int connectionRequestTimeout = 10000;
+            requestConfig = RequestConfig.custom().setConnectionRequestTimeout(
+                    connectionRequestTimeout).setSocketTimeout(socketTimeout).setConnectTimeout(
+                    connectTimeout).build();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+
+
+        // 设置请求超时时间
+        requestConfig = RequestConfig.custom().setSocketTimeout(50000).setConnectTimeout(50000)
+                .setConnectionRequestTimeout(50000).build();
+    }
+
+    public static CloseableHttpClient getHttpClient() {
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                // 设置连接池管理
+                .setConnectionManager(pool)
+                // 设置请求配置
+                .setDefaultRequestConfig(requestConfig)
+                // 设置重试次数
+                .setRetryHandler(new DefaultHttpRequestRetryHandler(0, false))
+                .build();
+
+        return httpClient;
+    }
+    private static String  sendHttpGet(HttpGet httpGet){
+        CloseableHttpClient httpClient = null;
+        CloseableHttpResponse response = null;
+        // 响应内容
+        String responseContent = null;
+        try {
+            // 创建默认的httpClient实例.
+            httpClient = getHttpClient();
+            // 配置请求信息
+            httpGet.setConfig(requestConfig);
+            // 执行请求
+            response = httpClient.execute(httpGet);
+            // 得到响应实例
+            HttpEntity entity = response.getEntity();
+
+            // 判断响应状态
+            if (response.getStatusLine().getStatusCode() >= 300) {
+                throw new Exception(
+                        "HTTP Request is not success, Response code is " + response.getStatusLine().getStatusCode());
+            }
+
+            if (HttpStatus.SC_OK == response.getStatusLine().getStatusCode()) {
+                responseContent = EntityUtils.toString(entity, CHARSET_UTF_8);
+                EntityUtils.consume(entity);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 释放资源
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return responseContent;
+    }
+    public static String HttpGet(String httpUrl){
+        //创建Get请求
+        HttpGet httpGet=new HttpGet(httpUrl);
+        return sendHttpGet(httpGet);
+    }
+}
