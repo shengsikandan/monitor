83ef3db752caefc1be438b3a71d4a95e4215cfa1
dt-tianba <455124491@qq.com>
2020-01-16 12:45:06 +0800

    更改返回值格式

--- src/main/java/com/prometheus/service/GetPrometheusDataService.java
+++ src/main/java/com/prometheus/service/GetPrometheusDataService.java
@@ -1,5 +1,7 @@
 package com.prometheus.service;
 
+import com.prometheus.Entry.CpuData;
+
 import java.util.HashMap;
 
 /**
@@ -11,5 +13,5 @@ public interface GetPrometheusDataService {
      * 获取具体的json数据，并对数据进行相应的处理后进行返回
      * @return
      */
-    HashMap<String,Object> getValue();
+    CpuData getValue();
 }
