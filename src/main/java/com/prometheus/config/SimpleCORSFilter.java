9a3878465824f9e1405398fa17bbbe10d8425492
dt-tianba <455124491@qq.com>
2020-01-16 12:30:00 +0800

    解决跨域问题

---
+++ src/main/java/com/prometheus/config/SimpleCORSFilter.java
@@ -0,0 +1,24 @@
+package com.prometheus.config;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 解决跨域问题
+ */
+@Component
+public class SimpleCORSFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        chain.doFilter(req, res);
+    }
+public void init(FilterConfig filterConfig) {} public void destroy() {} }
